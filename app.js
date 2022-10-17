ead89978940bdd8e4e4ad8a1453bde2706d83c4c
Lu de Oliveira <luludeolive.dev@gmail.com>
2022-10-17 10:12:50 -0300

    init repo

---
+++ app.js
@@ -0,0 +1,19 @@
+// grab a couple a thuings
+
+const section = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+const playerLives = 6;
+
+// link text
+
+playerLivesCount.textContent = playerLives;
+
+// generate the data
+
+const getData = () => [
+    {imgSrc: '.images/ventre-jm.png', name: 'ventre'},
+    
+
+];
+const data = getData();
+
